732c7a9c22bde416d2abb8412a82f1a76d759222
RIPSUDHAN MISHRA <ripusudhanpmishra@gmail.com>
2025-09-09 09:57:24 +0530

    changes in build folder

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import useThemeStore from '../store/useThemeStore';
 
-const StartPage = () => {
-  const { theme } = useThemeStore();    
+const StartPage = () => {   
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center p-8" data-theme={theme}>
+    <div className="flex flex-col items-center justify-center h-screen text-center p-8">
       <h1 className="text-3xl md:text-5xl font-extralight mb-4">Welcome to the Weather App</h1>
       <p className="text-base md:text-xl text-gray-400">Please search for a city to see the weather forecast.</p>
     </div>
